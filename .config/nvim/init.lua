6a24f4873150f20632168108162c9b51ac715903
Balazs Szendrodi <balazs.szendrodi@nokia.com>
2025-11-06 16:35:41 +0100

    remove mini.ai in favor of nvim-treesitter-textobjects

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,15 +7,16 @@ vim.pack.add({
   { src = "https://github.com/folke/tokyonight.nvim" },
   { src = "https://github.com/nvim-mini/mini.icons" },
   -- { src = "https://github.com/nvim-mini/mini.pick" },
-  {src = "https://github.com/ibhagwan/fzf-lua"}, -- alternate picker
+  { src = "https://github.com/ibhagwan/fzf-lua" }, -- alternate picker
   -- {src = "https://github.com/nvim-telescope/telescope.nvim"}, -- alternate picker
-  { src = "https://github.com/nvim-mini/mini.ai" },
+  -- { src = "https://github.com/nvim-mini/mini.ai" }, -- additional treesitter textobjects
   -- { src = "https://github.com/nvim-mini/mini.files" },
   { src = "https://github.com/nvim-lua/plenary.nvim" }, --dependency of yazi.nvim
   { src = "https://github.com/mikavilpas/yazi.nvim" },
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-context" }, -- Shows the the signiture of the funciton on the top while in the scope
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
   { src = "https://github.com/nvim-lualine/lualine.nvim" },
 })
 vim.cmd("colorscheme tokyonight-moon")
@@ -24,7 +25,7 @@ vim.cmd(":hi statusline guibg=NONE")
 -- Plugins
 require('plugins.mini.icons')
 -- require('plugins.mini.pick')
-require('plugins.mini.ai')
+-- require('plugins.mini.ai')
 -- require('plugins.mini.files') -- alternative to yazi.nvim with 0 dependencies
 require('plugins.fzf-lua')
 require('plugins.yazi')
